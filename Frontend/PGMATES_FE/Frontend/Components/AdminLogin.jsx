713829c4b286f39b1faea70a387c128e04d09c70
Atharva7057 <atharvapatil7057@gmail.com>
2025-01-07 19:17:54 +0530

    unwanted errors removed

--- Frontend/PGMATES_FE/Frontend/Components/AdminLogin.jsx
+++ Frontend/PGMATES_FE/Frontend/Components/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { verify } from "../Services/authenticate";
+
 
 function AdminLogin() {
   const queryParameters = new URLSearchParams(window.location.search);
