3ee54e0d3391a2781667a69054f5ce8355cce9f1
Atharva7057 <atharvapatil7057@gmail.com>
2025-02-10 12:08:31 +0530

    error resolving

--- Backend/PGmates_Backend/PGmates_Backend/PGMates/obj/Release/net8.0/PGMates.AssemblyInfo.cs
+++ Backend/PGmates_Backend/PGmates_Backend/PGMates/obj/Release/net8.0/PGMates.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PGMates")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713829c4b286f39b1faea70a387c128e04d09c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("PGMates")]
